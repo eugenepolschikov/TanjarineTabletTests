297b3f27ca6a70561ac2eaf6350789a15d64aac8
Siarhei <slezgouka@exadel>
2014-10-23 18:20:36 +0300

    test commit

--- borksTabletTestsLaunchScipt.bat
+++ borksTabletTestsLaunchScipt.bat
@@ -1,6 +1,7 @@
 cd  /d E:\exadel_projects\TouchtunesAutomation\TouchtunesAndroidSideTestsGitHubRepo\TanjarineTabletTests
 call android.bat create uitest-project -n TanjarineTabletTests -t 17 -p .
 call ant build
+
 adb  push ./bin/TanjarineTabletTests.jar /data/local/tmp
 adb shell uiautomator runtest TanjarineTabletTests.jar -c com.exadel.tanjarine.android.tests.NavigationTests
 pause
